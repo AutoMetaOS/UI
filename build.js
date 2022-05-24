827cced3d0c82d969ceddb36eddb01f051c90bce
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-05-24 17:39:31 +0530

    Add minifier

--- build.js
+++ build.js
@@ -1,4 +1,18 @@
+import { minify } from "html-minifier";
 import fs from "fs";
+
+const minify_config = {
+    removeAttributeQuotes: false,
+    removeRedundantAttributes: true,
+    removeEmptyElements: true,
+    removeEmptyAttributes: true,
+    removeComments: true,
+    preserveLineBreaks: false,
+    minifyCSS: true,
+    keepClosingSlash: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true
+};
 
 const maps = [
     { from: "./src", to: './prod/x/svg' },
@@ -9,9 +23,10 @@ const maps = [
 maps.forEach( ( { from, to } ) => {
     fs.readdirSync( from ).forEach( file => {
         if ( file.includes( '.svg' ) ) {
-            const fileData = fs.readFileSync( `${ from }/${ file }`, 'utf8' );
+            const file_data = fs.readFileSync( `${ from }/${ file }`, 'utf8' );
+            const result_file_data = minify( file_data, minify_config );
 
-            fs.writeFileSync( `${ to }/${ file }`, fileData );
+            fs.writeFileSync( `${ to }/${ file }`, result_file_data );
         };
     } );
 } );
